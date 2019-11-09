bf27d171d7072fc46dfaa76fd2d38c76033dd6bb
Slay Jay <ablosse1@mix.wvu.edu>
2019-11-09 00:15:23 -0500

    added build

---
+++ build/precache-manifest.6a19d57ce2fc67faf53a1df953f74bd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d93dfc208c6a0263c39d9ca33814ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54741aa642383e6b4e41",
+    "url": "/static/css/2.7ececfe9.chunk.css"
+  },
+  {
+    "revision": "77a894ae2a15124ab07f",
+    "url": "/static/css/main.2d653c6c.chunk.css"
+  },
+  {
+    "revision": "54741aa642383e6b4e41",
+    "url": "/static/js/2.6bb4db1c.chunk.js"
+  },
+  {
+    "revision": "77a894ae2a15124ab07f",
+    "url": "/static/js/main.d014e882.chunk.js"
+  },
+  {
+    "revision": "3f22f717c811ee790fce",
+    "url": "/static/js/runtime-main.7a041636.js"
+  }
+]);
