e5ba4b967f58afcf6094553c54b080e18ce7f60a
Slay Jay <ablosse1@mix.wvu.edu>
2019-11-09 00:26:21 -0500

    Updates

---
+++ precache-manifest.de09e26c4cc0288b4ff6f24c0d68abf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0efbe33d668110f9f4d66a3f6cc84df5",
+    "url": "/MegaCalc/index.html"
+  },
+  {
+    "revision": "a7d3b11d3db23ce2f0e8",
+    "url": "/MegaCalc/static/css/2.7ececfe9.chunk.css"
+  },
+  {
+    "revision": "7fe917fa20df1de05721",
+    "url": "/MegaCalc/static/css/main.59e370f5.chunk.css"
+  },
+  {
+    "revision": "a7d3b11d3db23ce2f0e8",
+    "url": "/MegaCalc/static/js/2.fd174f8d.chunk.js"
+  },
+  {
+    "revision": "7fe917fa20df1de05721",
+    "url": "/MegaCalc/static/js/main.7edf05e5.chunk.js"
+  },
+  {
+    "revision": "f7810d0f8371535077cc",
+    "url": "/MegaCalc/static/js/runtime-main.169faf63.js"
+  }
+]);
